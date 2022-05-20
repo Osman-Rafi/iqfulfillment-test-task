9bdcf12cde6ad42eca83e5ea7956bc4cb4e3d9f0
Osman Rafi <33856265+Osman-Rafi@users.noreply.github.com>
2022-05-20 21:02:11 +0600

    feat: sidebar moved to seperate component

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <b-button @click="toggleSidebar">
+    toggle</b-button>
+  </div>
+</template>
+
+<script>
+import { BButton } from "bootstrap-vue";
+export default {
+  name: "HeaderComponent",
+  components: {
+    BButton,
+  },
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {
+    toggleSidebar() {
+      this.$emit("toggleSidebar");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
