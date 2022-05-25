4ec4976e53afe6400814a7fed429774e3abb9a9c
Osman Rafi <33856265+Osman-Rafi@users.noreply.github.com>
2022-05-25 23:22:48 +0600

    feat: search api integrated

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,20 +9,67 @@
       >
         <i class="bi bi-list fs-3"></i>
       </b-button>
-      <b-form-group class="ms-md-4 me-3 ms-3">
-        <b-input-group class="mt-1">
-          <b-input-group-prepend>
-            <div class="search-box-prepend">
-              <i class="bi bi-search text-gray-100"></i>
-            </div>
-          </b-input-group-prepend>
-          <b-form-input
-            size="sm"
-            v-model="search"
-            placeholder="Search..."
-            class="search-box"
-          />
-          <b-input-group-append class="search-filter-toggler">
+      <div class="d-flex">
+        <div class="position-relative">
+          <b-form-group class="ms-md-4 ms-3">
+            <b-input-group class="mt-1">
+              <b-input-group-prepend
+                :class="{
+                  'input-group-prepend-with-suggestions':
+                    searchResults.length ||
+                    (!searchResults.length && search && !searchApiLoading),
+                }"
+              >
+                <div class="search-box-prepend">
+                  <i class="bi bi-search text-gray-100"></i>
+                </div>
+              </b-input-group-prepend>
+              <b-form-input
+                size="sm"
+                v-model="search"
+                placeholder="Search..."
+                class="search-box"
+                @update="searchInput"
+                @input="triggerInputChange"
+                debounce="500"
+              />
+              <div class="search-loading-spinner">
+                <b-spinner
+                  v-if="searchApiLoading"
+                  style="width: 1rem; height: 1rem"
+                ></b-spinner>
+              </div>
+            </b-input-group>
+          </b-form-group>
+          <div class="mx-3 ms-md-4 search-suggestions">
+            <template v-if="searchResults.length && search">
+              <div class="search-result-container bg-white">
+                <div
+                  v-for="(result, index) in searchResults"
+                  :key="index"
+                  class="result-item"
+                >
+                  <p class="fs-8 px-3 text-black-50 mb-0 mt-2">{{ result }}</p>
+                  <div class="px-3">
+                    <hr v-if="index !== searchResults.length - 1" />
+                    <div v-else class="pb-2"></div>
+                  </div>
+                </div>
+              </div>
+            </template>
+            <template
+              v-else-if="!searchResults.length && search && !searchApiLoading"
+            >
+              <div class="search-result-container bg-white">
+                <div class="result-item">
+                  <p class="fs-8 px-3 text-black-50 mb-1 mt-2">
+                    No items found
+                  </p>
+                </div>
+              </div>
+            </template>
+          </div>
+          <!-- <b-input-group-append class="search-filter-toggler">
             <b-dropdown
               class="search-dropdown"
               toggle-class="search-dropdown-toggle-button"
@@ -35,9 +82,21 @@
               </template>
               <b-dropdown-item>An item</b-dropdown-item>
             </b-dropdown>
-          </b-input-group-append>
-        </b-input-group>
-      </b-form-group>
+          </b-input-group-append> -->
+        </div>
+        <b-dropdown
+          class="search-dropdown"
+          toggle-class="search-dropdown-toggle-button"
+        >
+          <template #button-content>
+            <span class="text-black fw-7 ls-1">
+              Filter
+              <i class="bi bi-chevron-down text-gray-100 ms-2"></i>
+            </span>
+          </template>
+          <b-dropdown-item>An item</b-dropdown-item>
+        </b-dropdown>
+      </div>
     </div>
     <div class="mt-2 d-flex">
       <div
@@ -61,16 +120,18 @@
 </template>
 
 <script>
+import axios from "axios";
 import {
   BButton,
   BFormGroup,
   BInputGroup,
   BFormInput,
   BInputGroupPrepend,
-  BInputGroupAppend,
+  // BInputGroupAppend,
   BDropdown,
   BDropdownItem,
   BAvatar,
+  BSpinner,
 } from "bootstrap-vue";
 export default {
   name: "HeaderComponent",
@@ -80,53 +141,56 @@ export default {
     BFormInput,
     BInputGroup,
     BInputGroupPrepend,
-    BInputGroupAppend,
+    // BInputGroupAppend,
     BDropdown,
     BDropdownItem,
     BAvatar,
+    BSpinner,
   },
 
   data() {
     return {
       search: "",
+      searchResults: [],
+      searchApiLoading: false,
     };
   },
-
-  mounted() {},
 
   methods: {
     toggleSidebar() {
       this.$emit("toggleSidebar");
+    },
+    async searchInput() {
+      this.searchApiLoading = true;
+
+      if (this.search.length >= 2) {
+        try {
+          const response = await axios.post(
+            "https://apitest.iqfulfillment.com/v1/test/search",
+            { q: this.search }
+          );
+          this.searchResults = response.data.results;
+        } catch (error) {
+          console.log(error);
+        } finally {
+          this.searchApiLoading = false;
+        }
+      } else {
+        this.searchResults = [];
+        this.searchApiLoading = false;
+      }
+    },
+    triggerInputChange() {
+      this.searchApiLoading = !this.searchApiLoading;
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-@import "./header.scss";
+@import "./header-scoped.scss";
 </style>
 
 <style lang="scss">
-.search-dropdown {
-  height: 38px;
-  .dropdown-toggle {
-    background-color: #ffffff !important;
-    border-color: #ced4da !important;
-    border-radius: 0 18px 18px 0;
-    font-size: 10px;
-
-    &:hover,
-    &:focus,
-    &:active,
-    &:active:focus,
-    &:focus-visible {
-      background-color: #ffffff;
-      box-shadow: none;
-      border-color: #ced4da;
-    }
-  }
-}
-.search-filter-toggler {
-  margin-top: 8px;
-}
+@import "./header-global.scss";
 </style>
